c57cb3bb22fb8f42bf7bdc748ddbc144d5092d22
Annie9296 <nhinguyen.cfo@gmail.com>
2026-06-20 20:33:42 +0700

    Fix inventory calculation

--- Baitap13.java
+++ Baitap13.java
@@ -1,3 +1,23 @@
+
+abstract class CostOfGoodsSold{
+    String providerName;
+    enum ProviderType {
+        SUPPLIER, BROKER, WAREHOUSE
+    };
+    String shipmentId;
+    double shipmentAmount;
+    enum SupplierFeeType {
+        GoodsCost, PalletFee, Sorting
+    };
+    enum BrokerFeeType {
+        Loading, Discharge, Unloading
+    };
+    enum WarehouseFeeType {
+        Storage, Handling, Sorting
+    };
+    
+}
+
 public class Baitap13 {
     
 }
